319b2a60d8f9bdf2eb19e9df525ec2b42739ca36
Nicholas LaChance <nicholasplachance@gmail.com>
2024-01-23 13:12:42 -0600

    ADD service images

--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState } from 'react';
+
 import styled from 'styled-components';
 import theme from '../components/common/theme';
 
@@ -29,41 +30,73 @@ const ServiceDescription = styled.p`
   color: ${theme.colors.secondary};
 `;
 
+const ImageContainer = styled.div`
+  position: relative;
+  cursor: pointer;
+`;
+
+const FullScreenImage = styled.img`
+  width: 100%;
+  height: auto;
+  max-width: 100%;
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  margin: auto;
+  display: none; /* Initially hide the full-screen image */
+  z-index: 999;
+`;
+
 const Image = styled.img`
   width: 100%;
-  max-width: 600px;
-  height: auto;
+  height: 200px; /* Adjust the height as needed */
+  object-fit: cover;
+  object-position: center;
   margin-bottom: 1.5rem;
 `;
+
 
 const ServicesPage = () => {
   const services = [
     {
       title: 'Kitchen and Bathroom',
       description: 'Tact Construction is committed to your vision. Our team of designers work with you to design spaces that blend stunning aesthetics to fit your style,with functionality that suits your needs. Our skilled craftsmen take pride in precision and attention to detail all while maintaining a clean and safe workspace. We are prepared to turn your vision of a serene bathroom or elegant kitchen to life. ',
-      image: "../../public/assets/Portfolio/Lafayette Master Bath 5.jpg" 
+      image: "assets/Portfolio/Lafayette Master Bath 5.jpg"
     },
     {
       title: 'Whole House',
       description: "Re-envisioning your whole house allows you to unlock the full potential of your home, giving you the power to redefine the way you live. A comprehensive transformation can provide changes to your home's layout tailored to fit your needs and lifestyle, while providing you with the opportunity to customize every inch of your home. Our creative designers and skilled architects are experts at providing cohesive and harmonious designs that reflect your personality and enhance your daily life.",
-      image: "src\assets\Portfolio (1)\Walker Kitchen 1.jpg"
+      image: "assets/Portfolio/Walker Kitchen 1.jpg"
     },
     {
       title: 'CUSTOM BUILDS',
       description: 'From your vision to a turnkey home, Tact Construction and our expert team of contractors are equipped to handle every aspect of your project from blueprints and planning to the final coat of paint and landscaping. Our team of specialized designers and craftsmen are ready to bring your new home vision to life.',
-      image: "src\assets\Portfolio (1)\Hoard House 4.jpg"
+      image: "assets/Portfolio/Hoard House 4.jpg"
     },
     {
       title: 'ADDITIONS',
       description: 'Tact Construction can increase your living space for whatever suits your needs – more room to enjoy with family, space for entertaining, a home office, bigger kitchen or dining space, and so much more. This extra square footage immediately increases your property value and allows you to customize and re-envision your current home without the cost and headache of selling, building or buying, and moving to a new home. ',
+      image: "assets/Portfolio/Piles Kitchen 3.jpg"
     },
     {
       title: 'CUSTOM FINISHES',
       description: 'Elevate your home with luxurious custom finishes that fit your style and needs. Whether it’s turning your dreary laundry room into a chic and functional space or crafting a stylish home bar for entertaining guests, our team of designers are ready to bring every corner of your home to life. From stunning accent walls to transforming your untouched basement into a cozy living space, Tact Construction is ready to turn your vision into reality.',
+      image: "assets/Portfolio/Taca Accent Wall 4.jpg"
     },
     
     // Add more services as needed
   ];
+  const [fullScreenImage, setFullScreenImage] = useState(null);
+
+  const openFullScreen = (imageSrc) => {
+    setFullScreenImage(imageSrc);
+  };
+
+  const closeFullScreen = () => {
+    setFullScreenImage(null);
+  };
 
   return (
     <ServicesPageContainer>
@@ -72,11 +105,24 @@ const ServicesPage = () => {
         <ServiceCard key={index}>
           <ServiceTitle>{service.title}</ServiceTitle>
           <ServiceDescription>{service.description}</ServiceDescription>
-          <Image src={service.image} alt="Company Building" />
+          <ImageContainer onClick={() => openFullScreen(service.image)}>
+            <Image src={service.image} alt="Service Example" />
+          </ImageContainer>
         </ServiceCard>
       ))}
+
+      {/* Full-screen image */}
+      {fullScreenImage && (
+        <FullScreenImage
+          src={fullScreenImage}
+          alt="Full Screen"
+          onClick={closeFullScreen}
+        />
+      )}
     </ServicesPageContainer>
   );
 };
 
 export default ServicesPage;
+
+
