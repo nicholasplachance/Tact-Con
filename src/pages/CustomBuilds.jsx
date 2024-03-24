6962a1c7caacb58bc257000cc7d3198d1e69f226
nick lachance <nicholasplachance@gmail.com>
2024-03-23 21:48:56 -0500

    Change design credit from a tag to p

--- src/pages/CustomBuilds.jsx
+++ src/pages/CustomBuilds.jsx
@@ -28,34 +28,56 @@ const ServiceDescription = styled.p`
 
 
 const ImageContainer = styled.div`
+  width: 60vw;
   position: relative;
+  overflow: hidden;
+  border-radius: 8px;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+  transition: transform 0.3s ease-in-out;
+  display: flex;
+  flex-direction: column;
   cursor: pointer;
+
+  @media (max-width: 768px) {
+    max-width: 80vw;
+  }
+
+  &:hover {
+    transform: scale(1.05);
+
+    .photographer-credit-overlay {
+      opacity: 1;
+    }
+  }
 `;
 
 const Image = styled.img`
-  max-height: ${({ isExpanded }) => (isExpanded ? '100vh' : '650px')};
-  object-fit: contain;
-  object-position: center;
   width: 100%;
+  height: auto;
+  border-radius: 8px;
+  flex: 1;
 `;
-
 
 const PhotographerCredit = styled.div`
   position: absolute;
-  bottom: 8px;
-  left: 50%;
-  transform: translateX(-50%);
-  background-color: rgba(255, 255, 255, 0.8);
-  padding: 4px 8px;
-  border-radius: 4px;
-  font-size: 12px;
-  color: #333;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  opacity: 0;
+  background-color: rgba(0, 0, 0, 0.7);
+  color: white;
+  transition: opacity 0.3s ease-in-out;
 
-  a {
-    color: #333;
-    text-decoration: underline;
+  ${Image}:hover & {
+    opacity: 1;
   }
 `;
+
+
 
 const photographerCreditText = 'Designer: ';
 const photographerWebsite = 'https://www.heatherhannickdesigns.com/'; // Replace with actual photographer website
@@ -74,10 +96,14 @@ const CustomBuildsPage = () => {
         <ServiceDescription>
           From your vision to a turnkey home, Tact Construction and our expert team of contractors are equipped to handle every aspect of your project from blueprints and planning to the final coat of paint and landscaping. Our team of specialized designers and craftsmen are ready to bring your new home vision to life.
         </ServiceDescription>
-        <ImageCard
-  src="assets/Portfolio/Hoard House 4.jpg" alt="Custom Builds Example"
-  designer={{ name: "House of Hoard", website: "" }}
-/>
+        <ImageContainer>
+    <Image src="assets/Portfolio/Hoard House 4.jpg" alt="Custom Builds Example" />
+    <PhotographerCredit className="photographer-credit-overlay">
+      <p>
+      Designer: House of Hoard
+      </p>
+    </PhotographerCredit>
+  </ImageContainer>
         
       </ServiceCard>
     </ServicePageContainer>
